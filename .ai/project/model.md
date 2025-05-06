dc380d9c8847ea1f1d32e52cb0afb43b9bc275c3
lyl <liuyulong@defensedriver.com.cn>
2025-05-06 14:32:31 +0800

    init

---
+++ .ai/project/model.md
@@ -0,0 +1,18 @@
+device
+- id
+- name
+- ip
+- buildTimeStr
+- latestHeartBeatTime(timestamp)
+- status(0:离线，1:在线)
+
+
+event
+- id
+- dn
+- title
+- device_id
+- content
+- picstr
+- level(1:紧急,2:严重,3:轻微,4:警告)
+- status(0:清除,1:活动)
---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"github.com/dapr-platform/common"
+	daprd "github.com/dapr/go-sdk/service/http"
+	"github.com/go-chi/chi/v5"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	httpSwagger "github.com/swaggo/http-swagger"
+	"net/http"
+	"os"
+	"strconv"
+	_ "aibox-service/docs"
+)
+
+var (
+	PORT = 80
+)
+
+func init() {
+
+	if val := os.Getenv("LISTEN_PORT"); val != "" {
+		PORT, _ = strconv.Atoi(val)
+	}
+	common.Logger.Debug("use PORT ", PORT)
+}
+
+// @title aibox-service API
+// @version 1.0
+// @description aibox-service API
+// @BasePath /swagger/aibox-service
+func main() {
+	mux := chi.NewRouter()
+	mux.Handle("/metrics", promhttp.Handler())
+	mux.Handle("/swagger*", httpSwagger.WrapHandler)
+	s := daprd.NewServiceWithMux(":"+strconv.Itoa(PORT), mux)
+	common.Logger.Debug("server start")
+	if err := s.Start(); err != nil && err != http.ErrServerClosed {
+		common.Logger.Fatalf("error: %v", err)
+	}
+}
