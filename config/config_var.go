19f4fe40831703c0e55067764e88cfe37f6c1886
lyl <liuyulong@defensedriver.com.cn>
2025-05-15 02:34:04 +0800

    add delete expired events

--- config/config_var.go
+++ config/config_var.go
@@ -1,6 +1,10 @@
 package config
 
-import "os"
+import (
+	"os"
+
+	"github.com/spf13/cast"
+)
 
 var AUTO_UPGRADE = false
 var EVENT_EXPIRE_DAY = 3
@@ -10,6 +14,6 @@ func init() {
 		AUTO_UPGRADE = true
 	}
 	if os.Getenv("EVENT_EXPIRE_DAY") != "" {
-		EVENT_EXPIRE_DAY, _ = cast.ToInt(os.Getenv("EVENT_EXPIRE_DAY"))
+		EVENT_EXPIRE_DAY = cast.ToInt(os.Getenv("EVENT_EXPIRE_DAY"))
 	}
 }
